b21d8517a0d29ff54312e78b20f14c4f96603b79
Jaakko <jaakkopyrhonen@gmail.com>
2024-04-26 17:44:49 +0300

    updates for the network experiment

--- experiments/network_transmission/experiment.sh
+++ experiments/network_transmission/experiment.sh
@@ -10,16 +10,19 @@ mkdir -p ./measurement_data
 
 amount_transmissions=200
 
+python server_on_master.py $master_host $master_port &
+id=$!
+
 for payload_size in 200 400 600 800 1000 1200 1400
 do
     for wait_time in 2 4 8 10 12 14 16
     do
         measuretime=$(($wait_time * $amount_transmissions))
-        python server_on_master.py $((measuretime + 5)) $master_host $master_port $payload_size &
-        id=$!
         ssh ${user}@${host} "python transmit.py $payload_size $wait_time $amount_transmissions $master_host $master_port" &
         python ../../measurement.py ${bluetooth_addr} ${measuretime} 2 n > ./measurement_data/p$payload_size-w$wait_time.csv
-        kill -INT $id
         sleep 10
     done
 done
+
+# Stop the server
+kill -INT $id
--- experiments/network_transmission/server_on_master.py
+++ experiments/network_transmission/server_on_master.py
@@ -1,5 +1,4 @@
 import socket
-import time
 import logging
 import signal
 
@@ -7,35 +6,33 @@ FORMAT = "%(asctime)-15s %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger()
 
+RECV_MAX_SIZE=2048
+
 loop = True
 
 def sigint(sig, frame):
     global loop
     loop = False
 
-def listen(time_active: float, host: str, port: int, payload_size: int):
+def listen(host: str, port: int):
     signal.signal(signal.SIGINT, sigint) 
-    start_time = time.time()
-    curr_time = start_time
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen()
-        logging.log(logging.INFO, f"Listening on {host}:{port} for {time_active} seconds")
-        while (curr_time - start_time) < time_active and loop:
+        logging.log(logging.INFO, f"Listening on {host}:{port}")
+        # Will loop forever until sigint is catched
+        while loop:
             try:
                 conn, _ = s.accept()
                 with conn:
-                    data = conn.recv(payload_size)
+                    data = conn.recv(RECV_MAX_SIZE)
                     logging.log(logging.INFO, f"Received {len(data)} bytes.")
-                curr_time = time.time()
             except Exception as e:
                 print(logger.error(e))
 
 
 if __name__ == "__main__":
     from sys import argv
-    time_active = float(argv[1])
-    host = argv[2]
-    port = int(argv[3])
-    payload_size = int(argv[4])
-    listen(time_active, host, port, payload_size)
+    host = argv[1]
+    port = int(argv[2])
+    listen(host, port)
