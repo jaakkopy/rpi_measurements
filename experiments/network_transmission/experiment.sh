51e6d7bb4a4f5842abd965b4a5b9551b164f341a
Jaakko <jaakkopyrhonen@gmail.com>
2024-04-26 17:16:18 +0300

    updated network experiment

--- experiments/network_transmission/experiment.sh
+++ experiments/network_transmission/experiment.sh
@@ -10,16 +10,15 @@ mkdir -p ./measurement_data
 
 amount_transmissions=200
 
-for payload_size in 200 400 600 800 1000
+for payload_size in 200 400 600 800 1000 1200 1400
 do
-    for wait_time in 2 4 8 10 12
+    for wait_time in 2 4 8 10 12 14 16
     do
         measuretime=$(($wait_time * $amount_transmissions))
-        python server_on_master.py $((measuretime + 5)) $master_host $master_port &
-        ssh ${user}@${host} "python transmit.py $payload_size $wait_time $amount_transmissions $master_host $master_port" &
-        python ../../measurement.py ${bluetooth_addr} ${measuretime} 2 n > ./measurement_data/p$payload_size-w$wait_time.csv &
+        python server_on_master.py $((measuretime + 5)) $master_host $master_port $payload_size &
         id=$!
-        sleep ${measuretime}
+        ssh ${user}@${host} "python transmit.py $payload_size $wait_time $amount_transmissions $master_host $master_port" &
+        python ../../measurement.py ${bluetooth_addr} ${measuretime} 2 n > ./measurement_data/p$payload_size-w$wait_time.csv
         kill -INT $id
         sleep 10
     done
